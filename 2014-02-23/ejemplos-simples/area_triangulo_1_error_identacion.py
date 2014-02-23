ce193bf29a2abe6089bbe56b2d710241c09172bb
Antonio Ognio <aognio@rcp.pe>
2014-02-23 15:06:52 -0500

    Ejemplos muy sencillos de scripts en Python

---
+++ 2014-02-23/ejemplos-simples/area_triangulo_1_error_identacion.py
@@ -0,0 +1,3 @@
+def area_triangulo(base, altura):
+    resultado = base * altura / 2.0
+     return resultado
---
+++ 2014-02-23/ejemplos-simples/holamundo2.py
@@ -0,0 +1,5 @@
+def saludar():
+ print "Hola mundo!"
+
+if __name__ == '__main__':
+     saludar()
